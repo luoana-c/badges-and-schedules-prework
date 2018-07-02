40de29b734c41deb1836cb818e035382a67ffef4
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 05:06:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_names)
+  array_badges = []
+  for name in array_names
+    badge = "Hello, my name is #{name}."
+    array_badges.push(badge)
+  end
+  array_badges
+end
+
+def assign_rooms(array)
+  
+    assignment = "Hello, "
+  
+end
+
+def printer
+  
+end
+
+#rooms = (1..7).to_a
+ # room_assignments = []
